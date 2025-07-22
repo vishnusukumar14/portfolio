a496e04db6b3ab4d053fb47ed4395184edf3ba06
vishnusukumar14 <vishnuskky2001@gmail.com>
2025-07-22 13:49:32 +0530

    Add launchUri method

--- lib/core/util.dart
+++ lib/core/util.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-TextTheme createTextTheme(
-    BuildContext context, String bodyFontString, String displayFontString) {
-  TextTheme baseTextTheme = Theme.of(context).textTheme;
-  TextTheme bodyTextTheme = GoogleFonts.getTextTheme(bodyFontString, baseTextTheme);
-  TextTheme displayTextTheme =
-      GoogleFonts.getTextTheme(displayFontString, baseTextTheme);
-  TextTheme textTheme = displayTextTheme.copyWith(
-    bodyLarge: bodyTextTheme.bodyLarge,
-    bodyMedium: bodyTextTheme.bodyMedium,
-    bodySmall: bodyTextTheme.bodySmall,
-    labelLarge: bodyTextTheme.labelLarge,
-    labelMedium: bodyTextTheme.labelMedium,
-    labelSmall: bodyTextTheme.labelSmall,
-  );
-  return textTheme;
+class Utils {
+  static TextTheme createTextTheme(
+    BuildContext context,
+    String bodyFontString,
+    String displayFontString,
+  ) {
+    TextTheme baseTextTheme = Theme.of(context).textTheme;
+    TextTheme bodyTextTheme = GoogleFonts.getTextTheme(
+      bodyFontString,
+      baseTextTheme,
+    );
+    TextTheme displayTextTheme = GoogleFonts.getTextTheme(
+      displayFontString,
+      baseTextTheme,
+    );
+    TextTheme textTheme = displayTextTheme.copyWith(
+      bodyLarge: bodyTextTheme.bodyLarge,
+      bodyMedium: bodyTextTheme.bodyMedium,
+      bodySmall: bodyTextTheme.bodySmall,
+      labelLarge: bodyTextTheme.labelLarge,
+      labelMedium: bodyTextTheme.labelMedium,
+      labelSmall: bodyTextTheme.labelSmall,
+    );
+    return textTheme;
+  }
+
+  static Future<void> launch(String url) async {
+    final uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri, mode: LaunchMode.externalApplication);
+    } else {
+      debugPrint("Cannot launch URL: $url");
+    }
+  }
 }
